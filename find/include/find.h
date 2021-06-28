1f6394116113197647a79a1667a0ad18d16918b9
JooSouza3 <>
2021-06-28 22:14:27 +0000

    find

--- find/include/find.h
+++ find/include/find.h
@@ -11,14 +11,30 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam T para o value a ser comparado
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param value O valor a ser comparado na função
+ * @param eq Uma função que retorna true se o elemento é igual ao segundo, ou falso caso contrário. A assinatura da função de comparação deve ser bool eq( const Type &a, const Type &b)
+ *
+ * @return Um iterator(ou ponteiro) para o primeiro elemento que satisfaz a função eq
+ *
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
-    // TODO
-    return first;
-}
+    InputIt it1 = first;
+    InputIt it2 = last;
 
+    while(it1 != it2){
+      if(eq(*it1, value)){
+        return it1;
+      }
+      it1++;
+    }
+    return last;
+}
 }
 #endif
